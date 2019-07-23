e19bc1668fa78e40f0267e7bd7270230f19c4b7c
aste-github <34529552+aste-github@users.noreply.github.com>
2019-07-24 06:22:41 +0900

    DCT/IDCT/HPF/LPF: 画像サイズを可変に 画像全体にFilterかける関数を分割有無で2つ追加

--- ImgDCTfilter.py
+++ ImgDCTfilter.py
@@ -9,64 +9,71 @@ def basis(k, n, N):  # DCT行列作成用基底関数
     return c
 
 
-def DCT8x8(ImgArray8x8):  # DCT変換
-    N = 8
+def DCTNxN(ImgArrayNxN):  # DCT変換
+    N = ImgArrayNxN.shape[0]
     DCT = np.array([[basis(k, n, N) for n in range(N)] for k in range(N)])
-    buf = np.zeros((N, N))
-    np.dot(DCT, ImgArray8x8, out=buf)
+    buf = np.zeros_like(ImgArrayNxN)
+    np.dot(DCT, ImgArrayNxN, out=buf)
     np.dot(buf, DCT.T, out=buf)
     return buf
 
 
-def IDCT8x8(DCTArray8x8):  # IDCT変換
-    N = 8
+def IDCTNxN(DCTArrayNxN):  # IDCT変換
+    N = DCTArrayNxN.shape[0]
     DCT = np.array([[basis(k, n, N) for n in range(N)] for k in range(N)])
-    buf = np.zeros((N, N))
-    np.dot(DCT.T, DCTArray8x8, out=buf)
+    buf = np.zeros_like(DCTArrayNxN)
+    np.dot(DCT.T, DCTArrayNxN, out=buf)
     np.dot(buf, DCT, out=buf)
     return buf
 
 
-def DCTsave(DCTArray8x8, filename):  # DCT変換結果の画像保存
-    buf = np.round(np.abs(DCTArray8x8/8)).astype(np.uint8)
-    Image.fromarray(buf).save(filename)
+def LPF(DCTArray, border):
+    for y in range(DCTArray.shape[0]):
+        for x in range(DCTArray.shape[1]):
+            DCTArray[y][x] = DCTArray[y][x] if x+y < border else 0.
 
 
-def LPF(DCTArray8x8):
-    for y in range(8):
-        for x in range(8):
-            DCTArray8x8[y][x] = DCTArray8x8[y][x] if x+y < 8 else 0.
+def HPF(DCTArray, border):
+    for y in range(DCTArray.shape[0]):
+        for x in range(DCTArray.shape[1]):
+            DCTArray[y][x] = DCTArray[y][x] if x+y > border else 0.
 
 
-def HPF(DCTArray8x8):
-    for y in range(8):
-        for x in range(8):
-            DCTArray8x8[y][x] = DCTArray8x8[y][x] if x+y > 1 else 0.
+# ImgArrayをsizeの正方に分割して度合いborderのFilterをかける
+def DCTsplitFilter(ImgArray, size, Filter, border):
+    for y in range(0, ImgArray.shape[0]-size+1, size):
+        for x in range(0, ImgArray.shape[1]-size+1, size):
+            im_DCTNxN = DCTNxN(ImgArray[y:y+size, x:x+size])
+            Filter(im_DCTNxN, border)
+            im_IDCTNxN = IDCTNxN(im_DCTNxN)
+            stack_h = im_IDCTNxN if x == 0 \
+                else np.hstack((stack_h, im_IDCTNxN))
+        im_IDCT = stack_h if y == 0 else np.vstack((im_IDCT, stack_h))
+    return im_IDCT
 
 
-def addNoise(ImgArray):  # ごま塩ノイズ付加(1%)
+def DCTFilter(ImgArrayNxN, Filter, border):
+    return DCTsplitFilter(ImgArrayNxN, ImgArrayNxN.shape[0], Filter, border)
+
+
+def addNoise(ImgArray, ratio=0.01):  # ごま塩ノイズ付加(default:1%)
     y, x = ImgArray.shape
     s = x*y
-    for _ in range(s//100):
+    for _ in range(np.int(np.round(s*ratio))):
         ImgArray[random.randrange(0, y)][random.randrange(0, x)] \
             = random.randrange(2)*255.
 
 
 if __name__ == "__main__":
     # グレースケール化して読み込み、np.arrayに
-    im_before = np.array(Image.open('./lena.tiff').convert('L'), np.float)
+    im_orig = np.array(Image.open('./lena.tiff').convert('L'), np.float)
+    im_before = im_orig[:min(im_orig.shape), :min(im_orig.shape)]  # 正方化
     addNoise(im_before)  # ごま塩ノイズ付加
     # 処理前画像保存
     Image.fromarray(im_before.astype(np.uint8)).save('./before.tiff')
 
-    # 8x8をim_beforeの左上から右へ順に切り出す
-    for y in range(0, im_before.shape[0]-8+1, 8):
-        for x in range(0, im_before.shape[1]-8+1, 8):
-            im_DCT8x8 = DCT8x8(im_before[y:y+8, x:x + 8])
-            LPF(im_DCT8x8)  # Filter processing
-            im_IDCT8x8 = IDCT8x8(im_DCT8x8)
-            stack_h = im_IDCT8x8 if x == 0 \
-                else np.hstack((stack_h, im_IDCT8x8))
-        im_IDCT = stack_h if y == 0 else np.vstack((im_IDCT, stack_h))
+    # im_IDCT = DCTsplitFilter(im_before, 8, LPF, 8)
+    im_IDCT = DCTFilter(im_before, LPF, im_before.shape[0]//2)
+
     # 処理後画像保存
     Image.fromarray(np.round(im_IDCT).astype(np.uint8)).save('./after.tiff')
